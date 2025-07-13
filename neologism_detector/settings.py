6ae158317c5cd88df4be3ee46fe74691abe1ff35
brunomaroneze <bruno.maroneze@gmail.com>
2025-07-13 10:45:03 -0400

    Adiciona o diretório 'staticfiles' ao STATIC_ROOT e atualiza o requirements.txt com dependências necessárias.

--- neologism_detector/settings.py
+++ neologism_detector/settings.py
@@ -118,8 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    os.path.join(BASE_DIR, "static"),
 ]
 
 # Arquivos de media (para futuros uploads)
